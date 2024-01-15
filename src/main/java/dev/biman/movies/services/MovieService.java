32f448c0db6b743fb26a278705d599dc542a0581
Biman Gunarathne <bimangunarathne@outlook.com>
2024-01-15 09:07:44 +0530

    update three files MovieController, MovieRepository, MovieService

--- src/main/java/dev/biman/movies/services/MovieService.java
+++ src/main/java/dev/biman/movies/services/MovieService.java
@@ -2,10 +2,12 @@ package dev.biman.movies.services;
 
 import dev.biman.movies.models.Movie;
 import dev.biman.movies.repositories.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +15,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
     }
 }
